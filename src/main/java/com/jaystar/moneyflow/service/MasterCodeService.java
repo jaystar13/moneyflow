6a274b1b9e41fdf262d45fbed16bffb4600bc5c0
jaystar13 <astarisk2@gmail.com>
2021-10-24 07:58:38 +0900

    refactor: 클래스 이름변경

---
+++ src/main/java/com/jaystar/moneyflow/service/MasterCodeService.java
@@ -0,0 +1,36 @@
+package com.jaystar.moneyflow.service;
+
+import com.jaystar.moneyflow.domain.MasterCode;
+import com.jaystar.moneyflow.repository.MasterCodeRepository;
+
+import java.util.List;
+
+public class MasterCodeService {
+
+    private final MasterCodeRepository masterCodeRepository;
+
+    public MasterCodeService(MasterCodeRepository masterCodeRepository) {
+        this.masterCodeRepository = masterCodeRepository;
+    }
+
+    public List<MasterCode> getMasterCodes() {
+        return masterCodeRepository.findAll();
+    }
+
+    public MasterCode getMasterCodeById(long id) {
+        return masterCodeRepository.findById(id)
+                .orElseThrow(RuntimeException::new);
+    }
+
+    public MasterCode addMasterCode(MasterCode masterCode) {
+        return masterCodeRepository.save(masterCode);
+    }
+
+    public MasterCode updateMasterCodeName(long id, String masterCodeName) {
+        MasterCode masterCode = masterCodeRepository.findById(id).orElseThrow();
+
+        masterCode.updateCodeName(masterCodeName);
+
+        return masterCode;
+    }
+}
