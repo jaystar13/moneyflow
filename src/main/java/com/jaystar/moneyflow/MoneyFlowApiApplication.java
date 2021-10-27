893ae56de27568382e3c378c78afbba2cd2db987
jaystar13 <astarisk2@gmail.com>
2021-10-27 21:49:02 +0900

    feat: SpringBootApplication 클래스

---
+++ src/main/java/com/jaystar/moneyflow/MoneyFlowApiApplication.java
@@ -0,0 +1,12 @@
+package com.jaystar.moneyflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyFlowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyFlowApiApplication.class, args);
+    }
+}
