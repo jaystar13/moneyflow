4770a385afd8914ae402d758126284c7affd3f0b
lee.jaesung <lee.jaesung@joins.com>
2021-11-03 07:42:37 +0900

    fix: 마스터코드 엔티티, 빌더 패턴 적용

--- src/main/java/com/jaystar/moneyflow/domain/MasterCode.java
+++ src/main/java/com/jaystar/moneyflow/domain/MasterCode.java
@@ -1,19 +1,28 @@
 package com.jaystar.moneyflow.domain;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Id;
+
 @Getter
-@AllArgsConstructor
 public class MasterCode {
 
+    @Id
     @Setter
     private Long id;
 
     private String code;
 
     private String codeName;
+
+    @Builder
+    public MasterCode(Long id, String code, String codeName) {
+        this.id = id;
+        this.code = code;
+        this.codeName = codeName;
+    }
 
     public void modifyCodeName(String codeName) {
         this.codeName = codeName;
