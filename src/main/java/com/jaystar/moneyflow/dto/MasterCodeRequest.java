6d21e21d74808322323d6bd333ee573403eed345
lee.jaesung <lee.jaesung@joins.com>
2021-11-03 07:41:13 +0900

    feat: 마스터코드 DTO Class 추가

---
+++ src/main/java/com/jaystar/moneyflow/dto/MasterCodeRequest.java
@@ -0,0 +1,28 @@
+package com.jaystar.moneyflow.dto;
+
+import com.jaystar.moneyflow.domain.MasterCode;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class MasterCodeRequest {
+
+    private String code;
+
+    private String codeName;
+
+    @Builder
+    public MasterCodeRequest(String code, String codeName) {
+        this.code = code;
+        this.codeName = codeName;
+    }
+
+    public MasterCode toMasterCode() {
+        return MasterCode.builder()
+                .code(code)
+                .codeName(codeName)
+                .build();
+    }
+}
