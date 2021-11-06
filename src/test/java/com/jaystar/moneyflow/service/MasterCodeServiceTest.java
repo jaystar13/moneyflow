84e9d53ed0de3a405303992eaf82b6f4b63a7886
jaystar13 <astarisk2@gmail.com>
2021-11-07 07:16:36 +0900

    test: 마스터 코드명으로 조회 테스트 케이스 추가

--- src/test/java/com/jaystar/moneyflow/service/MasterCodeServiceTest.java
+++ src/test/java/com/jaystar/moneyflow/service/MasterCodeServiceTest.java
@@ -18,8 +18,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
@@ -80,6 +79,15 @@ class MasterCodeServiceTest {
 
         assertThatThrownBy(() -> masterCodeService.findMasterCode(0L))
                 .isInstanceOf(EntityNotFoundException.class);
+    }
+
+    @DisplayName("마스터코드명으로 조회한다.")
+    @Test
+    void containCodeName() {
+        given(masterCodeRepository.findByCodeNameContaining("코드1")).willReturn(anyList());
+
+        List<MasterCodeResponse> masterCodeResponses = masterCodeService.findByCodeNameContaining("코드1");
+        assertThat(masterCodeResponses).isNotNull();
     }
 
     @DisplayName("마스터 코드를 추가한다.")
