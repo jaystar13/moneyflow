f549ef142c61c3e4382dda14d347a303c99b514f
jaystar13 <astarisk2@gmail.com>
2021-11-07 07:06:07 +0900

    feat: 마스터 코드 추가 메서드 구현

--- src/main/java/com/jaystar/moneyflow/controller/MaterCodeController.java
+++ src/main/java/com/jaystar/moneyflow/controller/MaterCodeController.java
@@ -1,13 +1,16 @@
 package com.jaystar.moneyflow.controller;
 
+import com.jaystar.moneyflow.dto.MasterCodeRequest;
 import com.jaystar.moneyflow.dto.MasterCodeResponse;
 import com.jaystar.moneyflow.service.MasterCodeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("/master-codes")
 public class MaterCodeController {
 
     private final MasterCodeService masterCodeService;
@@ -16,8 +19,14 @@ public class MaterCodeController {
         this.masterCodeService = masterCodeService;
     }
 
-    @GetMapping("/master-codes")
+    @GetMapping
     public List<MasterCodeResponse> list() {
         return masterCodeService.findAllMasterCodes();
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> addMasterCode(@RequestBody MasterCodeRequest masterCodeRequest) {
+        Long saveId = masterCodeService.add(masterCodeRequest);
+        return ResponseEntity.created(URI.create("/master-codes/" + saveId)).build();
     }
 }
