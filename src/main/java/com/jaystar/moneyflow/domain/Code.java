c5ac1e926ea7074f2b30bdc3cc26bbc1a9a66415
jaystar13 <astarisk2@gmail.com>
2022-01-15 08:02:22 +0900

    feat: Code 도메인 추가

---
+++ src/main/java/com/jaystar/moneyflow/domain/Code.java
@@ -0,0 +1,26 @@
+package com.jaystar.moneyflow.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+public class Code {
+    @Id
+    private Long id;
+
+    private String name;
+
+    private Code parent;
+
+    private List<Code> child = new ArrayList<>();
+
+    public void addChildCode(Code child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
+}
