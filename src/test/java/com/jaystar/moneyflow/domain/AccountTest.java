914c327e0c355f506ba838aaa09ffbd07df16dda
lee.jaesung <lee.jaesung@joins.com>
2022-01-16 11:32:41 +0900

    test: 계좌테스트 코드 추가

---
+++ src/test/java/com/jaystar/moneyflow/domain/AccountTest.java
@@ -0,0 +1,27 @@
+package com.jaystar.moneyflow.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AccountTest {
+
+    @DisplayName("은행명을 가져온다.")
+    @Test
+    void bankName() {
+        //given
+        String bankName = "테스트은행";
+        Code bankCode = new Code();
+        bankCode.setId(1L);
+        bankCode.setName(bankName);
+
+        //when
+        Account account = new Account();
+        account.setId(1L);
+        account.setBankCode(bankCode);
+
+        //then
+        assertThat(account.bankName()).isEqualTo(bankName);
+    }
+}
