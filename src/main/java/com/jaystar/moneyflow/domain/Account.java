baa78e4f139e859cd3624830f60462076aa8d770
lee.jaesung <lee.jaesung@joins.com>
2022-01-16 11:33:06 +0900

    feat: 계좌 도메인 추가

---
+++ src/main/java/com/jaystar/moneyflow/domain/Account.java
@@ -0,0 +1,30 @@
+package com.jaystar.moneyflow.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class Account {
+
+    private Long id;
+
+    private String name;
+
+    private Code bankCode;
+
+    private String accountNo;
+
+    private Date fromDate;
+
+    private Date toDate;
+
+    private String definition;
+
+    public String bankName() {
+        return bankCode.getName();
+    }
+
+}
