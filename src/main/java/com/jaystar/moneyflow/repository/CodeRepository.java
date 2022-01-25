7720bb0a318071373c63602d4cf73f799eddcce7
lee.jaesung <lee.jaesung@joins.com>
2022-01-26 07:20:15 +0900

    feat: CodeRepository 추가

---
+++ src/main/java/com/jaystar/moneyflow/repository/CodeRepository.java
@@ -0,0 +1,7 @@
+package com.jaystar.moneyflow.repository;
+
+import com.jaystar.moneyflow.domain.Code;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CodeRepository extends JpaRepository<Code, Long> {
+}
