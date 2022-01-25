946d8f6b1a1fb4c8ac401acfb5bf3e119a1c6361
lee.jaesung <lee.jaesung@joins.com>
2022-01-26 07:20:56 +0900

    feat: Entity 어노테이션 추가 및 관계 설정

--- src/main/java/com/jaystar/moneyflow/domain/Code.java
+++ src/main/java/com/jaystar/moneyflow/domain/Code.java
@@ -3,23 +3,48 @@ package com.jaystar.moneyflow.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Setter
 @Getter
+@Entity
 public class Code {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "CODE_ID")
     private Long id;
 
     private String name;
 
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
     private Code parent;
 
+    @OneToMany(mappedBy = "parent")
     private List<Code> child = new ArrayList<>();
 
     public void addChildCode(Code child) {
         this.child.add(child);
         child.setParent(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Code code = (Code) o;
+        return Objects.equals(id, code.id) &&
+                Objects.equals(name, code.name) &&
+                Objects.equals(parent, code.parent) &&
+                Objects.equals(child, code.child);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, parent, child);
     }
 }
