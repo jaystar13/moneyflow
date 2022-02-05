535d2f634b7d69f8daa1cc81228f2721fa4405a4
jaystar13 <astarisk2@gmail.com>
2022-02-06 06:53:44 +0900

    feat: CodeController 기능 추가(수정/삭제/저장)

--- src/main/java/com/jaystar/moneyflow/code/controller/CodeController.java
+++ src/main/java/com/jaystar/moneyflow/code/controller/CodeController.java
@@ -1,11 +1,14 @@
 package com.jaystar.moneyflow.code.controller;
 
+import com.jaystar.moneyflow.code.dto.CodeRequest;
 import com.jaystar.moneyflow.code.dto.CodeResponse;
 import com.jaystar.moneyflow.code.service.CodeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -28,5 +31,23 @@ public class CodeController {
     @GetMapping("/{id}")
     public ResponseEntity<CodeResponse> findCode(@PathVariable Long id) {
         return ResponseEntity.ok(codeService.findCode(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateCode(@PathVariable Long id, @RequestBody CodeRequest codeRequest) {
+        codeService.updateCode(id, codeRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCode(@PathVariable Long id) {
+        codeService.deleteCode(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> addCode(@RequestBody @Valid CodeRequest codeRequest) {
+        Long saveId = codeService.save(codeRequest);
+        return ResponseEntity.created(URI.create("/api/codes/" + saveId)).build();
     }
 }
