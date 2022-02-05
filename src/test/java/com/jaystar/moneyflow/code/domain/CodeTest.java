008c2445ac9a717cbce10a4dad786ccc7111b41c
jaystar13 <astarisk2@gmail.com>
2022-02-06 07:16:15 +0900

    test: Code 도메인 수정에 의한 테스트 케이스 수정

--- src/test/java/com/jaystar/moneyflow/code/domain/CodeTest.java
+++ src/test/java/com/jaystar/moneyflow/code/domain/CodeTest.java
@@ -1,38 +1,16 @@
 package com.jaystar.moneyflow.code.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+class CodeTest {
 
-public class CodeTest {
-
-    @DisplayName("자식코드를 추가한다.")
+    @DisplayName("코드 객체 생성을 확인한다.")
     @Test
-    void addChildCodeTest() {
-        //given
+    void create() {
         Code code = new Code();
-        code.setId(1L);
-        code.setName("PARENT1");
 
-        Code childCode1 = new Code();
-        childCode1.setId(1L);
-        childCode1.setName("CHILD1");
-
-        Code childCode2 = new Code();
-        childCode2.setId(1L);
-        childCode2.setName("CHILD2");
-
-        Code childCode3 = new Code();
-        childCode3.setId(1L);
-        childCode3.setName("CHILD3");
-
-        //when
-        code.addChildCode(childCode1);
-        code.addChildCode(childCode2);
-
-        //then
-        assertThat(code.getChild()).contains(childCode1, childCode2);
-        assertThat(code.getChild()).doesNotContain(childCode3);
+        Assertions.assertThat(code).isEqualTo(new Code());
     }
 }
