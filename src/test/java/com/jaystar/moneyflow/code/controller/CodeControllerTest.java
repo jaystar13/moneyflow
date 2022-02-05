f765ebcc0415dd4ce9c777fba90440140ebd81f6
jaystar13 <astarisk2@gmail.com>
2022-02-06 06:53:06 +0900

    test: CodeController 테스트 케이스 추가(수정/삭제/저장)

--- src/test/java/com/jaystar/moneyflow/code/controller/CodeControllerTest.java
+++ src/test/java/com/jaystar/moneyflow/code/controller/CodeControllerTest.java
@@ -1,7 +1,10 @@
 package com.jaystar.moneyflow.code.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jaystar.moneyflow.code.dto.CodeRequest;
 import com.jaystar.moneyflow.code.dto.CodeResponse;
 import com.jaystar.moneyflow.code.service.CodeService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +17,13 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = CodeController.class)
 class CodeControllerTest {
@@ -28,6 +32,13 @@ class CodeControllerTest {
 
     @MockBean
     private CodeService codeService;
+
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
 
     @DisplayName("코드를 전체 조회한다.")
     @Test
@@ -75,6 +86,52 @@ class CodeControllerTest {
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("TEST9000")))
+                .andDo(print());
+    }
+
+    @DisplayName("코드를 수정한다.")
+    @Test
+    void updateCode() throws Exception {
+        CodeRequest codeRequest = CodeRequest.builder()
+                .name("TEST")
+                .build();
+
+        mockMvc.perform(put("/api/codes/{id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(codeRequest)))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @DisplayName("코드를 삭제한다.")
+    @Test
+    void deleteCode() throws Exception {
+        doNothing().when(codeService).deleteCode(any());
+
+        mockMvc.perform(delete("/api/codes/{id}", 1L))
+                .andExpect(status().isNoContent())
+                .andDo(print());
+    }
+
+    @DisplayName("코드를 생성한다.")
+    @Test
+    void addCode() throws Exception {
+        CodeRequest parent = CodeRequest.builder()
+                .name("parent")
+                .build();
+
+        CodeRequest codeRequest = CodeRequest.builder()
+                .name("TEST1")
+                .parent(parent)
+                .build();
+
+        given(codeService.save(any())).willReturn(1L);
+
+        mockMvc.perform(post("/api/codes")
+                        .content(objectMapper.writeValueAsString(codeRequest))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", "/api/codes/1"))
                 .andDo(print());
     }
 }
