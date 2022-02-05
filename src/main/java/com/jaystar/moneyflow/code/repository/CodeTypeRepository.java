bd4e848738857342adcc4a1592b5bdbb4a8b26ff
jaystar13 <astarisk2@gmail.com>
2022-02-06 08:14:26 +0900

    feat: CodeTypeRepository 구현

---
+++ src/main/java/com/jaystar/moneyflow/code/repository/CodeTypeRepository.java
@@ -0,0 +1,7 @@
+package com.jaystar.moneyflow.code.repository;
+
+import com.jaystar.moneyflow.code.domain.CodeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CodeTypeRepository extends JpaRepository<CodeType, Long> {
+}
