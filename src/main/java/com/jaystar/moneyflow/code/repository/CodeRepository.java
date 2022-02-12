7391a4f97453eb3c44f4e62c07e8286a4819e496
jaystar13 <astarisk2@gmail.com>
2022-02-13 07:20:49 +0900

    feat: 코드타입명으로 조회 메소드 추가

--- src/main/java/com/jaystar/moneyflow/code/repository/CodeRepository.java
+++ src/main/java/com/jaystar/moneyflow/code/repository/CodeRepository.java
@@ -2,9 +2,14 @@ package com.jaystar.moneyflow.code.repository;
 
 import com.jaystar.moneyflow.code.domain.Code;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CodeRepository extends JpaRepository<Code, Long> {
     List<Code> findByNameContaining(String contain);
+
+    @Query("SELECT c FROM Code c JOIN c.codeType ct WHERE c.codeType.name LIKE %:codeTypeName%")
+    List<Code> findByCodeTypeNameContaining(@Param("codeTypeName") String codeTypeName);
 }
