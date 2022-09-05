5f6526834148470373bb317ea966bc00ba73fb7c
lee.jaesung <lee.jaesung@joins.com>
2022-09-06 07:30:31 +0900

    feat: CharacterEncodingFilter 어노테이션 추가

---
+++ src/test/java/com/jaystar/moneyflow/config/EnableMockMvc.java
@@ -0,0 +1,22 @@
+package com.jaystar.moneyflow.config;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AutoConfigureMockMvc
+@Import(EnableMockMvc.Config.class)
+public @interface EnableMockMvc {
+    public class Config {
+        @Bean
+        public CharacterEncodingFilter characterEncodingFilter() {
+            return new CharacterEncodingFilter("utf-8", true);
+        }
+    }
+}
